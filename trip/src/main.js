a09b3d0c2a8b821e143d2b86062cbbed1428ca66
zyzyzyk <1805250680@qq.com>
2019-07-30 14:34:54 +0800

    Trip

--- trip/src/main.js
+++ trip/src/main.js
@@ -6,8 +6,14 @@ import router from './router'
 import mandMobile from 'mand-mobile'
 import 'mand-mobile/lib/mand-mobile.css'
 import store from './store/index'
+import AMap from "vue-amap";
 
 Vue.use(mandMobile)
+Vue.use(AMap)
+AMap.initAMapApiLoader({
+  key: 'd2019952471b2f7bad0605b3328c4177',
+  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor']
+})
 // 请求拦截器
 import requestPlugin from './request/http.js'
 Vue.use(requestPlugin)
